3a8de0147d6c3ae0ede975296dabfcb5d0aa7295
Z_Itachi <zafar.bin.rahmat@gmail.com>
2025-10-19 01:59:56 +0530

    Initial Commit

---
+++ BattleBucksAssignment/BattleBucksAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleBucksAssignmentApp.swift
+//  BattleBucksAssignment
+//
+//  Created by Zafar Bin Rahmat on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BattleBucksAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
